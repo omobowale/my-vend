5d01291a0db50eaaa3831f77c673b1cd8358a381
Abiodun <adeyinkab24@gmail.com>
2021-06-08 19:38:42 +0100

    finished phase

--- src/components/common/TruckIcon/PaypalLogo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import paypal from '../../../assets/img/brands/paypal.png';
-
-function PaypalLogo({ className }) {
-  return <img src={paypal} alt="paypal" className={className} />;
-}
-
-export default PaypalLogo;
